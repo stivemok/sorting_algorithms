ee1f25afe8e022e2ed8867e8682d862d3b0e9503
stivemok <stivemok@gmail.com>
2023-04-24 06:22:05 -0700

    mandatory

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -11,22 +11,40 @@ void quick_sort(int *array, size_t size)
 }
 
 /**
+* lomuto - Lomuto partition
+* @array: Array to be sorted
+* @lower_bund: Lower index of partition array
+* @upper_bund: Higher index of partition array
+* @size: Size of array
+*/
+void lomuto(int *array, int lower_bund, int upper_bund, size_t size)
+{
+	int prtn;
+
+	if (lower_bund > upper_bund)
+		return;
+	prtn = swap_partition(array, lower_bund, upper_bund, size);
+	lomuto(array, lower_bund, prtn - 1, size);
+	lomuto(array, prtn + 1, upper_bund, size);
+}
+
+/**
  * swap_partition - Swaps arrays in a partition
  * @array: Array to be sorted
- * @low: Lower index of partition array
- * @high: Higher index of partition array
+ * @lower_bund: Lower index of partition array
+ * @upper_bund: Higher index of partition array
  * @size: Size of array
  * Return: Partition index position
  */
-int swap_partition(int *array, int low, int high, size_t size)
+int swap_partition(int *array, int lower_bund, int upper_bund, size_t size)
 {
 	int i, k, temp, pivot;
 
-	i = low;
-	k = low - 1;
-	pivot = array[high];
+	i = lower_bund;
+	k = lower_bund - 1;
+	pivot = array[upper_bund];
 
-	while (i <= high)
+	while (i <= upper_bund)
 	{
 		if (array[i] <= pivot)
 		{
@@ -42,22 +60,4 @@ int swap_partition(int *array, int low, int high, size_t size)
 		i++;
 	}
 	return (k);
-}
-
-/**
-* lomuto - Lomuto partition
-* @array: Array to be sorted
-* @low: Lower index of partition array
-* @high: Higher index of partition array
-* @size: Size of array
-*/
-void lomuto(int *array, int low, int high, size_t size)
-{
-	int prtn;
-
-	if (low > high)
-		return;
-	prtn = swap_partition(array, low, high, size);
-	lomuto(array, low, prtn - 1, size);
-	lomuto(array, prtn + 1, high, size);
 }
