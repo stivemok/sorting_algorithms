a2d32364467c5b690ff7fd25e8f63a700158cfd4
stivemok <stivemok@gmail.com>
2023-04-25 09:02:23 -0700

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,22 +7,22 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, k;
+	size_t i, j, min;
 	int temp;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		k = i;
+		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[k] > array[j])
-				k = j;
+			if (array[j] < array[min])
+				min = j;
 		}
-		if (i != k)
+		if (i != min)
 		{
 			temp = array[i];
-			array[i] = array[k];
-			array[k] = temp;
+			array[i] = array[min];
+			array[min] = temp;
 			print_array(array, size);
 		}
 	}
